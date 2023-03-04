e83c2d183db283cc56d3c254f4f27e4a4780f48e
Santiago Mattiauda <santimattius@gmail.com>
2023-03-04 18:00:26 -0300

    refactor: use toast

--- app/src/main/java/com/santimattius/template/ui/home/HomeFragment.kt
+++ app/src/main/java/com/santimattius/template/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -22,7 +23,9 @@ class HomeFragment : Fragment() {
     private val viewModel: HomeViewModel by viewModels()
 
     private val homeAdapter: PopularMoviesAdapter by lazy {
-        PopularMoviesAdapter { openLink(it.imageUrl) }
+        PopularMoviesAdapter {
+            Toast.makeText(requireContext(), it.title, Toast.LENGTH_SHORT).show()
+        }
     }
 
     private lateinit var viewBinding: PopularMoviesFragmentBinding
