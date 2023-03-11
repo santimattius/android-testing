b5c681cac005dbc9ee521cb9f79ca7aad9699b0f
Santiago Mattiauda <santimattius@gmail.com>
2023-03-11 17:23:25 -0300

    di: test refactor

--- app/src/test/java/com/santimattius/template/data/repositories/TMDbRepositoryTest.kt
+++ app/src/test/java/com/santimattius/template/data/repositories/TMDbRepositoryTest.kt
@@ -1,58 +1,43 @@
 package com.santimattius.template.data.repositories
 
-import android.content.Context
-import android.os.Build
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.santimattius.template.MainApplication
-import com.santimattius.template.data.datasources.implementation.MovieDataSource
-import com.santimattius.template.data.datasources.implementation.RoomDataSource
+import com.santimattius.template.data.datasources.LocalDataSource
+import com.santimattius.template.data.datasources.RemoteDataSource
 import com.santimattius.template.data.dtoToEntity
 import com.santimattius.template.utils.MainCoroutinesTestRule
 import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.*
-import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(AndroidJUnit4::class)
-@Config(manifest = Config.NONE, sdk = [Build.VERSION_CODES.R])
 class TMDbRepositoryTest {
 
     @get:Rule
     val coroutinesTestRule = MainCoroutinesTestRule()
 
-    private val app = ApplicationProvider.getApplicationContext<MainApplication>()
-    private lateinit var repository: TMDbRepository
-
-    @Before
-    fun setUp() {
-        mockkConstructor(MovieDataSource::class)
-        mockkConstructor(RoomDataSource::class)
-        repository = TMDbRepository(app)
-    }
+    private val remoteDataSource = mockk<RemoteDataSource>()
+    private val localDataSource = mockk<LocalDataSource>()
+    private val repository: TMDbRepository = TMDbRepository(remoteDataSource, localDataSource)
 
     @After
     fun tearDown() {
-        unmockkConstructor(MovieDataSource::class)
-        unmockkConstructor(RoomDataSource::class)
+        clearMocks(remoteDataSource, localDataSource)
     }
 
     @Test
-    @Ignore("Is bad test")
     fun `first test example`() {
         val movies = MovieMother.getMovies()
         //Given
         coEvery {
-            anyConstructed<MovieDataSource>().getPopularMovies()
+            remoteDataSource.getPopularMovies()
         } returns Result.success(movies)
+
         coEvery {
-            constructedWith<RoomDataSource>(OfTypeMatcher<Context>(Context::class)).save(any())
+            localDataSource.save(any())
         } returns Result.success(true)
+
         coEvery {
-            constructedWith<RoomDataSource>(OfTypeMatcher<Context>(Context::class)).getAll()
+            localDataSource.getAll()
         } returns movies.dtoToEntity()
 
         //When
@@ -62,13 +47,13 @@ class TMDbRepositoryTest {
 
         //Then
         coVerify {
-            anyConstructed<MovieDataSource>().getPopularMovies()
+            remoteDataSource.getPopularMovies()
         }
         coVerify {
-            constructedWith<RoomDataSource>(OfTypeMatcher<Context>(Context::class)).save(any())
+            localDataSource.save(any())
         }
         coVerify {
-            constructedWith<RoomDataSource>(OfTypeMatcher<Context>(Context::class)).getAll()
+            localDataSource.getAll()
         }
     }
 }
