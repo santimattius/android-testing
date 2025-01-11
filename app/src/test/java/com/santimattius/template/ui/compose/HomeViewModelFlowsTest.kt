1674796abe7e58a0d82d6cea95ec520f4d2dac77
Santiago Mattiauda <santimattius@gmail.com>
2025-01-11 15:15:10 -0300

    [feature] implement viewModelScenario

--- app/src/test/java/com/santimattius/template/ui/compose/HomeViewModelFlowsTest.kt
+++ app/src/test/java/com/santimattius/template/ui/compose/HomeViewModelFlowsTest.kt
@@ -1,5 +1,9 @@
 package com.santimattius.template.ui.compose
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.testing.ViewModelScenario
+import androidx.lifecycle.viewmodel.testing.viewModelScenario
 import app.cash.turbine.test
 import com.santimattius.shared_test.data.MovieMother
 import com.santimattius.shared_test.data.dtoToDomain
@@ -13,7 +17,6 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Rule
 import org.junit.Test
 
-//TODO: use view model scenario
 class HomeViewModelFlowsTest {
 
     @get:Rule
@@ -22,44 +25,52 @@ class HomeViewModelFlowsTest {
     @Test
     fun `Given result is success When popular movies Then return movies`() {
         val movies = MovieMother.createMovies().dtoToDomain()
-        runTest(mainCoroutinesTestRule.testDispatcher) {
-            val viewModel = HomeViewModel(FakeMovieRepository())
-            viewModel.state.test {
-                assertThat(
-                    awaitItem(),
-                    equalTo(HomeState.Data(movies.asUiModels()))
-                )
-                cancelAndIgnoreRemainingEvents()
+        val repository = FakeMovieRepository(onPopularMovies = { movies })
+        viewModelScenario { HomeViewModel(repository) }.use { scenario ->
+            val viewModel = scenario.viewModel
+            runTest(mainCoroutinesTestRule.testDispatcher) {
+                viewModel.state.test {
+                    assertThat(
+                        awaitItem(),
+                        equalTo(HomeState.Data(movies.asUiModels()))
+                    )
+                    cancelAndIgnoreRemainingEvents()
+                }
             }
         }
     }
 
     @Test
     fun `Given result is empty When popular movies Then return empty list`() {
-        runTest(mainCoroutinesTestRule.testDispatcher) {
-            val viewModel = HomeViewModel(FakeMovieRepository(onPopularMovies = { emptyList() }))
-            viewModel.state.test {
-                assertThat(
-                    awaitItem(),
-                    equalTo(HomeState.Empty)
+        val repository = FakeMovieRepository(onPopularMovies = { emptyList() })
+        viewModelScenario { HomeViewModel(repository) }.use {
+            val viewModel = it.viewModel
+            runTest(mainCoroutinesTestRule.testDispatcher) {
+                viewModel.state.test {
+                    assertThat(
+                        awaitItem(),
+                        equalTo(HomeState.Empty)
 
-                )
-                cancelAndIgnoreRemainingEvents()
+                    )
+                    cancelAndIgnoreRemainingEvents()
+                }
             }
         }
     }
 
     @Test
     fun `Given result is error When popular movies Then return error`() {
-        runTest(mainCoroutinesTestRule.testDispatcher) {
-            val viewModel =
-                HomeViewModel(FakeMovieRepository(onPopularMovies = { throw Exception() }))
-            viewModel.state.test {
-                assertThat(
-                    awaitItem(),
-                    equalTo(HomeState.Error)
-                )
-                cancelAndIgnoreRemainingEvents()
+        val repository = FakeMovieRepository(onPopularMovies = { throw Exception() })
+        viewModelScenario { HomeViewModel(repository) }.use { scenario ->
+            val viewModel = scenario.viewModel
+            runTest(mainCoroutinesTestRule.testDispatcher) {
+                viewModel.state.test {
+                    assertThat(
+                        awaitItem(),
+                        equalTo(HomeState.Error)
+                    )
+                    cancelAndIgnoreRemainingEvents()
+                }
             }
         }
     }
