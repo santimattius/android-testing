bd8e8aeb7ca6ff4f0d0163d41b40aa24ee19bfce
Santiago Mattiauda <santimattius@gmail.com>
2025-02-12 20:38:08 -0300

    [improvement] kotzilla plugin init

--- app/src/main/java/com/santimattius/template/MainApplication.kt
+++ app/src/main/java/com/santimattius/template/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.santimattius.template
 
 import android.app.Application
+import io.kotzilla.sdk.analytics.koin.analytics
 import io.kotzilla.sdk.analytics.koin.analyticsLogger
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.logger.AndroidLogger
@@ -16,7 +17,7 @@ class MainApplication : Application(), KoinStartup {
     override fun onKoinStartup(): KoinConfiguration {
         return KoinConfiguration {
             androidContext(this@MainApplication)
-            analyticsLogger()
+            analytics(this@MainApplication)
             logger(AndroidLogger())
             modules(
                 com_santimattius_template_di_DataModule,
